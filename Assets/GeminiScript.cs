d24ae69aac515c44fba1ed92aba4790755a4cef4
Timwi <github@timwi.de>
2022-03-01 06:19:36 +0100

    Fixes
    (and also, keyboard input)

--- Assets/GeminiScript.cs
+++ Assets/GeminiScript.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using KModkit;
 using UnityEngine;
 using Rnd = UnityEngine.Random;
-using KModkit;
-using System.Collections.Generic;
 
 public class GeminiScript : MonoBehaviour
 {
@@ -24,17 +24,15 @@ public class GeminiScript : MonoBehaviour
 
     private int[] _startingNums = new int[3];
     private int[] _currentNums = new int[3];
-    private string SerialNumber;
-    private int[][] _snIxs = new int[3][] { new int[3], new int[3], new int[3] };
-    private int[][] _snThreeDigits = new int[3][] { new int[3], new int[3], new int[3] };
     private int[] _functionOffsets = new int[3];
 
-    private Coroutine _cycleText;
     private bool _inputStarted;
-    private string _inputString = "---------";
+    private string _inputString;
     private int _timer;
     private bool _timerStarted;
     private int[] _inputNums = new int[3];
+    private bool _gonnaStrike;
+    private bool _hasFocus;
 
     private class GeminiInfo
     {
@@ -52,15 +50,17 @@ public class GeminiScript : MonoBehaviour
             ButtonSels[i].OnInteract += ButtonPress(i);
         GoSel.OnInteract += GoPress;
 
-        SerialNumber = BombInfo.GetSerialNumber();
-        if (!_infos.ContainsKey(SerialNumber))
-            _infos[SerialNumber] = new GeminiInfo();
-        _info = _infos[SerialNumber];
+        GetComponent<KMSelectable>().OnFocus += delegate { _hasFocus = true; };
+        GetComponent<KMSelectable>().OnDefocus += delegate { _hasFocus = false; };
+
+        var sn = BombInfo.GetSerialNumber();
+        if (!_infos.ContainsKey(sn))
+            _infos[sn] = new GeminiInfo();
+        _info = _infos[sn];
         if (ModuleName == "Castor")
             _info.CastorModules.Add(this);
         if (ModuleName == "Pollux")
             _info.PolluxModules.Add(this);
-        StartCoroutine(Init());
 
         for (int i = 0; i < _startingNums.Length; i++)
         {
@@ -69,48 +69,57 @@ public class GeminiScript : MonoBehaviour
             ScreenTexts[i].text = _currentNums[i].ToString("000");
         }
         Debug.LogFormat("[{0} #{1}] Starting numbers: {2}", ModuleName, _moduleId, _startingNums.Join(", "));
-        var snArr = SerialNumber.Select(i => i >= '0' && i <= '9' ? i - '0' : i - 'A' + 1).ToArray();
-        newOffset:
-        for (int i = 0; i < _functionOffsets.Length; i++)
+
+        StartCoroutine(Init());
+    }
+
+    private void ResetModule()
+    {
+        _timerStarted = false;
+        _inputStarted = false;
+        _gonnaStrike = false;
+        _inputString = "---------";
+
+        _timer = Rnd.Range(30, 71);
+        if (_partner != null && _partner._timer != 0)
+            while (Math.Abs(_timer - _partner._timer) < 20)
+                _timer = Rnd.Range(20, 81);
+        TimerText.text = _timer.ToString();
+
+        do
         {
-            _snIxs[i] = Enumerable.Range(0, 6).ToArray().Shuffle().Take(3).ToArray();
-            for (int j = 0; j < 3; j++)
-                _snThreeDigits[i][j] = snArr[_snIxs[i][j]];
-            _functionOffsets[i] = _snThreeDigits[i][0] * _snThreeDigits[i][1] + _snThreeDigits[i][2];
+            for (int i = 0; i < _functionOffsets.Length; i++)
+                _functionOffsets[i] = Rnd.Range(1, 100);
         }
-        if (_functionOffsets.Distinct().Count() != 3 || _functionOffsets.Contains(0))
-            goto newOffset;
+        while (_functionOffsets.Distinct().Count() != 3);
+
         for (int i = 0; i < 3; i++)
-        {
-            Debug.LogFormat("[{0} #{1}] Function {2} serial number positions: {3}, {4}, {5}.", ModuleName, _moduleId, "ABC"[i], _snIxs[i][0] + 1, _snIxs[i][1] + 1, _snIxs[i][2] + 1);
-            Debug.LogFormat("[{0} #{1}] Function {2} value: n {3} {4}.", ModuleName, _moduleId, "ABC"[i], ModuleName == "Castor" ? "+" : "-", _functionOffsets[i]);
-        }
-        _cycleText = StartCoroutine(CycleText());
+            Debug.LogFormat("[{0} #{1}] Function {2} offset: n {3} {4}.", ModuleName, _moduleId, "ABC"[i], ModuleName == "Castor" ? "+" : "-", _functionOffsets[i]);
+        Debug.LogFormat("[{0} #{1}] Possible input: {2}", ModuleName, _moduleId, _functionOffsets.Select(i => (ModuleName == "Pollux" ? i * _timer % 1000 : (1000 - i) * _timer % 1000).ToString("000")).Join(" "));
     }
 
     private IEnumerator Init()
     {
+        // Give all modules a chance to add themselves to the CastorModules/PolluxModules lists
         yield return null;
+
+        // Discover my own partner
         var ix = (ModuleName == "Castor" ? _info.CastorModules : _info.PolluxModules).IndexOf(this);
         var pList = ModuleName == "Pollux" ? _info.CastorModules : _info.PolluxModules;
         _partner = pList.Count > ix ? pList[ix] : null;
+
+        if (_partner != null)
+            Debug.LogFormat("[{0} #{1}] This module has a partner!", ModuleName, _moduleId);
+        else
+            Debug.LogFormat("[{0} #{1}] This module does not have a partner.", ModuleName, _moduleId);
+
+        // Display ID
         var idTxt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[ix / 26].ToString() + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[ix % 26].ToString();
         IdText.text = idTxt;
         Debug.LogFormat("[{0} #{1}] This module's ID is {2}", ModuleName, _moduleId, idTxt);
-        _timer = Rnd.Range(30, 71);
-        if (_partner != null)
-        {
-            Debug.LogFormat("[{0} #{1}] This module has a partner!", ModuleName, _moduleId);
-            if (_partner._timer != 0)
-                while (Math.Abs(_timer - _partner._timer) < 20)
-                    _timer = Rnd.Range(20, 81);
-        }
-        else
-        {
-            Debug.LogFormat("[{0} #{1}] This module does not have a partner.", ModuleName, _moduleId);
-        }
-        Debug.LogFormat("[{0} #{1}] Possible input: {2}", ModuleName, _moduleId, _functionOffsets.Select(i => ModuleName == "Pollux" ? i * _timer % 1000 : (1000 - i) * _timer % 1000).Join(" "));
-        TimerText.text = _timer.ToString();
+
+        ResetModule();
+        StartCoroutine(CycleText());
     }
 
     private KMSelectable.OnInteractHandler ButtonPress(int btn)
@@ -154,6 +163,10 @@ public class GeminiScript : MonoBehaviour
     {
         while (!_moduleSolved)
         {
+            int time = (int) BombInfo.GetTime();
+            while (time == (int) BombInfo.GetTime())
+                yield return null;
+
             if (!_inputStarted)
             {
                 for (int i = 0; i < _currentNums.Length; i++)
@@ -161,72 +174,59 @@ public class GeminiScript : MonoBehaviour
                     _currentNums[i] = FunctionEncode(_currentNums[i], i);
                     ScreenTexts[i].text = _currentNums[i].ToString("000");
                 }
+                goto checks;
             }
-            else if (_timerStarted)
-            {
-                if (_timer == 0)
-                {
-                    yield return null;
-                    if (!CheckAnswer())
-                    {
-                        yield return null;
-                        _timerStarted = false;
-                        _inputStarted = false;
-                        _timer = Rnd.Range(30, 71);
-                        TimerText.text = _timer.ToString();
-                    }
-                }
-                else
-                {
-                    for (int i = 0; i < _currentNums.Length; i++)
-                    {
-                        _inputNums[i] = FunctionEncode(_inputNums[i], i);
-                        ScreenTexts[i].text = _inputNums[i].ToString("000");
-                    }
-                    _timer--;
-                    TimerText.text = _timer.ToString();
-                }
-            }
-            int time = (int)BombInfo.GetTime();
-            while (time == (int)BombInfo.GetTime())
-                yield return null;
-            if (_partner != null && _partner._timer == 0 && _timer != 0)
-            {
-                yield return null;
-                if (_partner._moduleSolved)
-                {
-                    Module.HandleStrike();
-                    Debug.LogFormat("[{0} #{1}] This module's partner solved prematurely. Strike.", ModuleName, _moduleId);
-                }
-                else
-                    Debug.LogFormat("[{0} #{1}] Stopping the timer on this module because its partner's timer ran out and struck.", ModuleName, _moduleId);
-                Debug.LogFormat("[{0} #{1}] Regenerating...", ModuleName, _moduleId);
-                _timerStarted = false;
-                _inputStarted = false;
-                _partner = null;
-                _timer = Rnd.Range(30, 71);
-                Debug.LogFormat("[{0} #{1}] Possible input: {2}", ModuleName, _moduleId, _functionOffsets.Select(i => ModuleName == "Pollux" ? i * _timer % 1000 : (1000 - i) * _timer % 1000).Join(" "));
-                TimerText.text = _timer.ToString();
-            }
-        }
-    }
 
-    private bool CheckAnswer()
-    {
-        if (_inputNums.Distinct().Count() == 1)
-        {
-            _moduleSolved = true;
-            Module.HandlePass();
-            if (_cycleText != null)
-                StopCoroutine(_cycleText);
-            Debug.LogFormat("[{0} #{1}] All three numbers are equal the end of the timer. Module solved.", ModuleName, _moduleId);
-            return true;
-        }
-        else
-        {
-            Module.HandleStrike();
-            Debug.LogFormat("[{0} #{1}] The three numbers ({2}) are not equal at the end of the timer. Strike.", ModuleName, _moduleId, _inputNums.Join(", "));
-            return false;
+            if (!_timerStarted)
+                goto checks;
+
+            if (_timer == 0)
+            {
+                if (_inputNums.Distinct().Count() == 1)
+                {
+                    _moduleSolved = true;
+                    Module.HandlePass();
+                    Debug.LogFormat("[{0} #{1}] All three numbers are equal the end of the timer. Module solved.", ModuleName, _moduleId);
+                    yield break;
+                }
+                else
+                    _gonnaStrike = true;
+            }
+            else
+            {
+                _timer--;
+                TimerText.text = _timer.ToString();
+
+                for (int i = 0; i < _currentNums.Length; i++)
+                {
+                    _inputNums[i] = FunctionEncode(_inputNums[i], i);
+                    ScreenTexts[i].text = _inputNums[i].ToString("000");
+                }
+            }
+
+            checks:
+            // Give the partner a chance to solve or mark _gonnaStrike
+            yield return null;
+
+            if (_gonnaStrike)
+            {
+                Module.HandleStrike();
+                Debug.LogFormat("[{0} #{1}] The three numbers ({2}) are not equal at the end of the timer. Strike.", ModuleName, _moduleId, _inputNums.Join(", "));
+            }
+            else if (_partner != null && _partner._moduleSolved)
+            {
+                Module.HandleStrike();
+                Debug.LogFormat("[{0} #{1}] This module’s partner solved prematurely. Strike.", ModuleName, _moduleId);
+                _partner = null;
+            }
+            else if (_partner != null && _partner._gonnaStrike && _timerStarted)
+                Debug.LogFormat("[{0} #{1}] This module’s partner’s timer has run out and struck. Reset.", ModuleName, _moduleId);
+            else
+                continue;
+
+            // Reset the module
+            yield return null;
+            ResetModule();
         }
     }
 
@@ -243,5 +243,13 @@ public class GeminiScript : MonoBehaviour
                 break;
         }
         return (val + 1000) % 1000;
+    }
+
+    private void Update()
+    {
+        if (_hasFocus)
+            for (int i = 0; i <= 9; i++)
+                if (Input.GetKeyDown(KeyCode.Alpha0 + i) || Input.GetKeyDown(KeyCode.Keypad0 + i))
+                    ButtonSels[i].OnInteract();
     }
 }
